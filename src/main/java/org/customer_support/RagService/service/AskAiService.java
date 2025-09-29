043f5bd0500baa52a000c57b48433aec288b3dac
harshil8705 <harshil8705@users.noreply.github.com>
2025-09-29 22:41:10 +0530

    Made RAG System more Accurate

--- src/main/java/org/customer_support/RagService/service/AskAiService.java
+++ src/main/java/org/customer_support/RagService/service/AskAiService.java
@@ -3,6 +3,7 @@ package org.customer_support.RagService.service;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.template.st.StTemplateRenderer;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
@@ -20,23 +21,31 @@ public class AskAiService {
     public String chatResponse(String userQuery) {
 
         PromptTemplate customPromptTemplate = PromptTemplate.builder()
+                .renderer(StTemplateRenderer.builder().startDelimiterToken('<').endDelimiterToken('>').build())
                 .template("""
-                Answer the query strictly referring to the provided context:
-                
-                Context:
-                {context}
-                
-                Query:
-                {query}
-                
-                Rules:
-                1. If the answer is not in the context, just say you don't know.
-                2. Avoid statements like "Based on the context..." or "The provided information...".
-                """).build();
+                    <query>
+        
+                    Context information is below.
+        
+			        ---------------------
+			        <question_answer_context>
+			        ---------------------
+        
+			        Given the context information and no prior knowledge, answer the query.
+        
+			        Follow these rules:
+        
+			        1. If the answer is not in the context, just say that you don't know.
+			        2. Avoid statements like "Based on the context..." or "The provided information...".
+                    """)
+                .build();
 
-        return chatClient.prompt()
-                .user(userQuery)
-                .advisors(new QuestionAnswerAdvisor(vectorStore))
+        QuestionAnswerAdvisor qaAdvisor = QuestionAnswerAdvisor.builder(vectorStore)
+                .promptTemplate(customPromptTemplate)
+                .build();
+
+        return chatClient.prompt(userQuery)
+                .advisors(qaAdvisor)
                 .call()
                 .content();
 
